9ceea85fffd6cdf7c7423082757577bb5fd051fe
PBX_g33k <git@yu-go.eu>
2012-11-06 22:54:53 +0100

    meh

--- test.php
+++ test.php
@@ -5,7 +5,6 @@ require_once( AMFPHP_BASE . 'amf/io/AMFDeserializer.php');
   include_once('ustreamrip.class.php');
   $rip = new Ustreamrip();
   $rip->Init();
-//  $rip->setChannel('dj-kentai-jcore-hardcore-mix');
   $rip->setChannel('bear-lake-golden-doodles-puppy-cam-2');
   $data = $rip->getRTMPCommand();
   var_dump($data);
